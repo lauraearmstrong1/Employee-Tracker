a94c50713a077c7213a15a9c4c6bcaf642663612
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-04-16 11:09:39 -0400

    view roles and departments

--- seeds.sql
+++ seeds.sql
@@ -11,15 +11,26 @@ VALUES
 INSERT INTO role
   (title, salary, department_id)
 VALUES
-  ('Sales Lead', 100000, 1);
+  ('Sales Lead', 100000, 1),
+  ('Salesperson', 80000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Engineer', 120000, 2),
+  ('Account Manager', 160000, 3),
+  ('Accountant', 125000, 3),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 190000, 4);
 
 INSERT INTO employee
   (first_name, last_name, role_id, manager_id)
 VALUES
-  ('John', 'Doe', 1, NULL);
-
-
-
+  ('John', 'Doe', 1, 3),
+  ('Mike', 'Chan', 2, 1),
+  ('Ashley', 'Rodriguez', 3, NULL),
+  ('Kevin', 'Tupik', 4, 3),
+  ('Malia', 'Brown', 6, NULL),
+  ('Sarah', 'Lourd', 7, NULL),
+  ('Tom', 'Allen', 8, 6),
+  ('Tammer', 'Galal', 4, 4);
 
 SELECT * FROM department;
 SELECT * FROM role;
--- server.js
+++ server.js
@@ -29,56 +29,67 @@ function makeDepartment() {
         message: "What would you like to do?",
         choices: [
             "View All Employees",
-            "View All Employees by Department",
-            "View All Employees by Manager",
-            "Add Employee",
-            "Remove Employee",
-            "Update Employee Role",
-            "Update Employee Manager",
             "View All Roles",
+            "View All Departments",
+            "Add Employee",
+            "Add Department",
             "Add Role",
-            "Remove Role"]
+            "Update Employee Role",
+            // "View All Employees by Department",
+            // "View All Employees by Manager",
+            // "Update Employee Manager",
+            // "Remove Employee",
+            // "Remove Role"
+        ]
     }).then(function (answer) {
         switch (answer.typeOfChange) {
             case "View All Employees":
-                allEmployees();
-                break;
-
-            case "View All Employees by Department":
-                department();
-                break;
-
-            case "View All Employees by Manager":
-                manager();
-                break;
-
-            case "Add Employee":
-                addEmployee();
-                break;
-
-            case "Remove Employee":
-                removeEmployee();
-                break;
-
-            case "Update Employee Role":
-                updateRole();
-                break;
-
-            case "Update Employee Manager":
-                updateManager();
+                viewEmployees();
                 break;
 
             case "View All Roles":
                 viewRoles();
                 break;
 
+            case "View All Departments":
+                viewDepartments();
+                break;
+
+            case "Add Employee":
+                addEmployee();
+                break;
+
+            case "Add Department":
+                addDepartment();
+                break;
+
             case "Add Role":
                 addRole();
                 break;
 
-            case "REmove Role":
-                removeRole();
+            case "Update Employee Role":
+                updateRole();
                 break;
+
+            // case "View All Employees by Department":
+            //     allByDepartment();
+            //     break;
+
+            // case "View All Employees by Manager":
+            //     manager();
+            //     break;
+
+            // case "Remove Employee":
+            //     removeEmployee();
+            //     break;
+
+            // case "Update Employee Manager":
+            //     updateManager();
+            //     break;
+
+            // case "Remove Role":
+            //     removeRole();
+            //     break;
 
             case "exit":
                 connection.end();
@@ -87,7 +98,7 @@ function makeDepartment() {
     });
 };
 
-function allEmployees() {
+function viewEmployees() {
     var query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
 
     connection.query(query, function (err, res) {
@@ -101,14 +112,30 @@ function allEmployees() {
     });
 };
 
+function viewRoles() {
+    var query = "SELECT role.title AS roles FROM role;"
 
+    connection.query(query, function (err, res) {
+        if (err) throw err;
 
-function department() {
+        console.table(res)
 
+        makeDepartment();
+
+    });
 }
 
-function manager() {
+function viewDepartments() {
+    var query = "SELECT department.name AS department FROM department;"
 
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res)
+
+        makeDepartment();
+
+    });
 }
 
 function addEmployee() {
@@ -123,73 +150,52 @@ function addEmployee() {
             }))
 
             connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
-            (err, res)=>{
-                if(err) throw err
-                managerz = res.map(({id, first_name, last_name})=>({
-                    name: first_name+ " "+ last_name,
-                    value: id
-                }))
-            managerz.push ({name: "No Manager", value: null})
-            inquirer.prompt([
-                {
-                    type: "input",
-                    name: "first_name",
-                    message: "What is the employee's first name?"
-                },
-                {
-                    type: "input",
-                    name: "last_name",
-                    message: "What is the employee's last name?"
-                },
-                {
-                    type: "list",
-                    name: "role_id",
-                    message: "What is the employees role?",
-                    choices: rolez
-                },
-                {
-                    type: "list",
-                    name: "manager_id",
-                    message: "Who is the employees manager?",
-                    choices: managerz
-                },
-            ]).then(res => {
-               // console.log(res)
-                connection.query("INSERT INTO employee Set ?", res,
-                (err, res)=>{
-                    if(err) throw err
-                    console.log("Employee Added")
-                    makeDepartment();
+                (err, res) => {
+                    if (err) throw err
+                    managerz = res.map(({ id, first_name, last_name }) => ({
+                        name: first_name + " " + last_name,
+                        value: id
+                    }))
+                    managerz.push({ name: "No Manager", value: null })
+
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "first_name",
+                            message: "What is the employee's first name?"
+                        },
+                        {
+                            type: "input",
+                            name: "last_name",
+                            message: "What is the employee's last name?"
+                        },
+                        {
+                            type: "list",
+                            name: "role_id",
+                            message: "What is the employees role?",
+                            choices: rolez
+                        },
+                        {
+                            type: "list",
+                            name: "manager_id",
+                            message: "Who is the employees manager?",
+                            choices: managerz
+                        },
+                    ]).then(res => {
+                        console.log(res)
+                        connection.query("INSERT INTO employee Set ?", res,
+                            (err, res) => {
+                                if (err) throw err
+                                console.log("Employee Added")
+                                makeDepartment();
+                            })
+                    })
                 })
-            })
-        })})
-    //var query = "INSERT INTO employee SET ?", employee
-
-}
-//.map(({ id, first_name, last_name }) => ({
-//name: `${first_name} ${last_name}`
-
-//   connection.query(query, function (err, res) {
-//   })
-
-//         makeDepartment();
-
-//     });
-// }
-
-function removeEmployee() {
+        })
 
 }
 
-function updateRole() {
-
-}
-
-function updateManager() {
-
-}
-
-function viewRoles() {
+function addDepartment() {
 
 }
 
@@ -197,12 +203,8 @@ function addRole() {
 
 }
 
-function removeRole() {
+function updateRole() {
 
 }
 
 
-
-
-
-//makeDepartment();
